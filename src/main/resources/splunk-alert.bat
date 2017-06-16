17d7c5d1376c3f454b07f9d25e12b7b7d8786d60
satish.muddam <satish.muddam@appdynamics.com>
2017-06-16 18:18:47 +0530

    Removing hard coded java path w.r.t controller

--- src/main/resources/splunk-alert.bat
+++ src/main/resources/splunk-alert.bat
@@ -2,4 +2,4 @@
 rem ------------------------------------------------
 rem Batch file to send event notifications to Splunk
 rem ------------------------------------------------
-..\..\..\jre/bin/java -Dlog4j.configuration=file:conf/log4j.xml -jar splunk-alerting-extension.jar %*
+java -Dlog4j.configuration=file:conf/log4j.xml -jar splunk-alerting-extension.jar %*
